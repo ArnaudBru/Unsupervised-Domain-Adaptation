f2c72ecc81d9bce1e21700cf451561a5530eb8cd
ArnaudBru <brugiere.arnaud@gmail.com>
2021-05-24 17:09:30 +0200

    Create metrics.py
    Functions to calculate accuracy and accuracy by class

---
+++ utils/metrics.py
@@ -0,0 +1,41 @@
+def accuracy(model, dataloader, verbose=False):
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for data in dataloader:
+            images, labels = data[0].to(device), data[1].to(device)
+            # calculate outputs by running images through the network
+            outputs = model(images)
+            # the class with the highest energy is what we choose as prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    if verbose:
+      print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+    else:
+      return correct/total
+
+def class_accuracy(model, dataloader, classes, verbose=False):
+    # prepare to count predictions for each class
+    correct_pred = {classname: 0 for classname in classes}
+    total_pred = {classname: 0 for classname in classes}
+
+    # again no gradients needed
+    with torch.no_grad():
+        for data in dataloader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = model(images)
+            _, predictions = torch.max(outputs, 1)
+            # collect the correct predictions for each class
+            for label, prediction in zip(labels, predictions):
+                if label == prediction:
+                    correct_pred[classes[label]] += 1
+                total_pred[classes[label]] += 1
+
+
+    # print accuracy for each class
+    for classname, correct_count in correct_pred.items():
+        accuracy = 100 * float(correct_count) / total_pred[classname]
+        print(f"Accuracy for class {classname} is: {accuracy:.1f}% ({total_pred[classname]} elements)")
