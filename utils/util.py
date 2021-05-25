f1ab8a01219d16156cbf930085fdf802b174b69c
ArnaudBru <brugiere.arnaud@gmail.com>
2021-05-25 17:27:20 +0200

    Update util.py
    Add generated_sample function: shows example of generated images

--- utils/util.py
+++ utils/util.py
@@ -1,3 +1,6 @@
+import torch
+from torch.autograd import Variable
+
 import matplotlib.pyplot as plt
 
 def data_sample(dataloader, sample_size=5):
@@ -9,3 +12,22 @@ def data_sample(dataloader, sample_size=5):
         ax[i].imshow(-1*image.reshape(size).numpy(), cmap='Greys')
         ax[i].title.set_text(f'True Label: {target[i]}')
       break
+
+def generated_sample(generator, n_classes, latent_dim):
+  f, ax = plt.subplots(1,n_classes, figsize=(15,15))
+
+  device = next(generator.parameters()).device
+  generator.eval()
+
+  z = Variable(torch.randn(n_classes, latent_dim)).to(device)
+  gen_labels = Variable(torch.LongTensor(np.arange(n_classes))).to(device)
+
+  gen_imgs = generator(z, gen_labels).view(-1,1,img_size, img_size)
+  size = gen_imgs.size()[-2:]
+
+  for i, image in enumerate(gen_imgs):
+    image = image.cpu().detach()
+    ax[i].imshow(-1*image.numpy().reshape(size), cmap='Greys')
+    ax[i].set_title(gen_labels[i].item())
+  plt.show()
+
