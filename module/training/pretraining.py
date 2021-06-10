a6ae2a87c846296ca7a6e223747c46299465274e
ArnaudBru <brugiere.arnaud@gmail.com>
2021-06-10 14:47:13 +0200

    Update pretraining.py
    Add gan_pretraining

--- module/training/pretraining.py
+++ module/training/pretraining.py
@@ -3,11 +3,16 @@ Pretraining functions for classifier and GAN
 """
 import os
 
+import numpy as np
+
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 import torch.optim as optim
 
-from module.training.training_step import classifier_train_step
+from torchvision.utils import save_image
+
+from module.training.training_step import classifier_train_step, generator_train_step, discriminator_train_step
 from module.util.metrics import accuracy
 
 
@@ -60,3 +65,86 @@ def cls_pretraining(classifier, loader_train, loader_test, learning_rate, n_epoc
     torch.save(classifier.state_dict(), cls_pretrained_path)
 
     return classifier
+
+
+def gan_pretraining(generator, discriminator, classifier, loader_train,
+                    lr_g, lr_d, latent_dim, n_classes, n_epochs,
+                    img_size, results_path):
+    """
+    Args:
+        generator (TYPE)
+        discriminator (TYPE)
+        classifier (TYPE)
+        loader_train (TYPE)
+        lr_g (TYPE)
+        lr_d (TYPE)
+        latent_dim (TYPE)
+        n_classes (TYPE)
+        n_epochs (TYPE)
+        img_size (TYPE)
+        results_path (TYPE)
+
+    Returns:
+        Pretrained generator and discriminator
+    """
+    img_pretraining_path = ''.join([results_path, '/images'])
+    models_pretraining_path = ''.join([results_path, '/gan_models'])
+
+    g_pretrained = ''.join([results_path, '/generator_pretrained.pth'])
+    d_pretrained = ''.join([results_path, '/discriminator_pretrained.pth'])
+
+    device = next(classifier.parameters()).device
+
+    criterion_gan = nn.BCELoss().to(device)
+    d_optimizer = torch.optim.Adam(discriminator.parameters(), lr=lr_d)
+    g_optimizer = torch.optim.Adam(generator.parameters(), lr=lr_g)
+
+    os.makedirs(img_pretraining_path, exist_ok=True)
+    os.makedirs(models_pretraining_path, exist_ok=True)
+
+    loaded_gen = False
+    loaded_dis = False
+
+    if os.path.isfile(g_pretrained):
+        generator.load_state_dict(torch.load(g_pretrained))
+        print('loaded existing generator')
+        loaded_gen = True
+
+    if os.path.isfile(d_pretrained):
+        discriminator.load_state_dict(torch.load(d_pretrained))
+        print('loaded existing discriminator')
+        loaded_dis = True
+
+    if not(loaded_gen and loaded_dis):
+        for epoch in range(n_epochs):
+            print(f'Starting epoch {epoch}/{n_epochs}...', end=' ')
+            g_loss_list = []
+            d_loss_list = []
+            for i, (images, _) in enumerate(loader_train):
+
+                real_images = Variable(images).to(device)
+                _, labels = torch.max(classifier(real_images), dim=1)
+
+                generator.train()
+
+                d_loss = discriminator_train_step(discriminator, generator, d_optimizer, criterion_gan,
+                                                  real_images, labels, latent_dim, n_classes)
+                d_loss_list.append(d_loss)
+
+                g_loss = generator_train_step(discriminator, generator, g_optimizer, criterion_gan,
+                                              loader_train.batch_size, latent_dim, n_classes=n_classes)
+                g_loss_list.append(g_loss)
+
+            generator.eval()
+
+            latent_space = Variable(torch.randn(n_classes, latent_dim)).to(device)
+            gen_labels = Variable(torch.LongTensor(np.arange(n_classes))).to(device)
+
+            gen_imgs = generator(latent_space, gen_labels).view(-1, 1, img_size, img_size)
+            save_image(gen_imgs.data, img_pretraining_path + f'/epoch_{epoch:02d}.png', nrow=n_classes, normalize=True)
+            torch.save(generator.state_dict(), models_pretraining_path + f'/{epoch:02d}_gen.pth')
+            torch.save(discriminator.state_dict(), models_pretraining_path + f'/{epoch:02d}_dis.pth')
+
+            print(f"[D loss: {np.mean(d_loss_list)}] [G loss: {np.mean(g_loss_list)}]")
+
+    return generator, discriminator
